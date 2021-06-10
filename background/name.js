277a4330b9856aafa9dd213a71d40c394f895925
Lionel Wong <lionelster@gmail.com>
2021-06-10 21:08:41 +0800

    Simplify uniquify and applyNumberPostfix

--- background/name.js
+++ background/name.js
@@ -16,18 +16,11 @@ export function validify(str) {
 // If window name str is not unique, add number postfix to it.
 // Supply excludeId to skip a particular window to check against.
 export function uniquify(str, excludeId) {
-    while (true) {
-        const error = hasName(str, excludeId);
-        if (!error) break;
-        str = applyNumberPostfix(str);
-    }
-    return str;
+    return hasName(str, excludeId) ? uniquify(applyNumberPostfix(str)) : str;
 }
 
 // Add " 2" at the end of str, or increment an existing " number".
 export function applyNumberPostfix(str) {
     const found = str.match(numberPostfix);
-    return found
-        ? `${str.slice(0, found.index)} ${parseInt(found[1]) + 1}`
-        : str + ` 2`;
+    return found ? `${str.slice(0, found.index)} ${Number(found[1]) + 1}` : `${str} 2`;
 }
